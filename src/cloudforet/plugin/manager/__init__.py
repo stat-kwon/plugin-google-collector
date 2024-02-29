f1a9c6b4f883437cc47ff402ce3ee8a83bd55c17
seolmin <seolmin@megazone.com>
2024-02-29 14:12:06 +0900

    fix: fix typo

--- src/cloudforet/plugin/manager/__init__.py
+++ src/cloudforet/plugin/manager/__init__.py
@@ -1,4 +1,5 @@
-# from cloudforet.plugin.manager.base import ResourceManager
+from cloudforet.plugin.manager.base import ResourceManager
+
 # from cloudforet.plugin.manager.pub_sub.schema import SchemaManager
 # from cloudforet.plugin.manager.pub_sub.snapshot import SnapshotManager
 # from cloudforet.plugin.manager.pub_sub.subscription import SubscriptionManager
